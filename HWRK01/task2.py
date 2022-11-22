900cf82e8d8018135dd0152281168bca61ebe673
Solea <cassandra01@yandex.ru>
2022-11-22 23:32:54 +0300

    дз №1, дополнение

--- HWRK01/task2.py
+++ HWRK01/task2.py
@@ -1,7 +1,11 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+def assignValue(value):
+    return int(value) == 1 or False
 
-x = int(input('Введите х (0 или 1): ')) == 1 or False
-y = int(input('Введите y (0 или 1): ')) == 1 or False
-z = int(input('Введите z (0 или 1): ')) == 1 or False
+x=assignValue(input('Введите х (0 или 1): '))
+y=assignValue(input('Введите y (0 или 1): '))
+z=assignValue(input('Введите z (0 или 1): '))
+
 print(f'Полученные предикаты: {x}, {y}, {z}')
 print('Результат - ' + str(not (x and y and z) == ((not x) or (not y) or (not z))))
+

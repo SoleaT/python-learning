47280d7600c243c50ae6231a5d87ca5fd15eec5e
Solea <cassandra01@yandex.ru>
2022-11-29 12:51:01 +0300

    дз2, задача 1, 2 варианта решения

---
+++ HWRK02/task1.py
@@ -0,0 +1,9 @@
+print('Сумма цифр: ', sum(int(i) for i in input('Введите число: ') if i != '.'), sep=' ')
+
+# :)
+# s = input('Введите число: ')
+# sm=0
+# for i in s:
+#     if i!='.' and i!=',':
+#         sm+=int(i)
+# print('Сумма цифр:',sm,sep=' ')

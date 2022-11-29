1941dcd5fba6e4b0b7407c9786f090c07a6dd511
Solea <cassandra01@yandex.ru>
2022-11-29 13:26:17 +0300

    дз2, задача 1, 2 варианта решения

--- HWRK02/task1.py
+++ HWRK02/task1.py
@@ -1,9 +1,20 @@
-print('Сумма цифр: ', sum(int(i) for i in input('Введите число: ') if i != '.'), sep=' ')
+# Способ1, Запись1
+print('Сумма цифр:', sum(int(i) for i in input('Введите число: ') if i.isdigit()))
 
-# :)
-# s = input('Введите число: ')
-# sm=0
-# for i in s:
-#     if i!='.' and i!=',':
-#         sm+=int(i)
-# print('Сумма цифр:',sm,sep=' ')
+# :) Способ1, Запись2
+s = input('Введите число: ')
+sm=0
+for i in s:
+    if i!='.' and i!=',':
+        sm+=int(i)
+print('Сумма цифр:',sm)
+
+# Способ2.
+
+s=[int(i) for i in input('Введите число: ').split('.')]
+sm = 0
+for num in s:
+    while num>0:
+        sm+=num%10
+        num//=10
+print('Сумма цифр:', sm)

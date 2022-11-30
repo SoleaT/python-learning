26e89e725c2fc30930f74685d3fa1e824f5dfdfc
Solea <cassandra01@yandex.ru>
2022-11-30 23:08:23 +0300

    дз3, 4 задачи

---
+++ HWRK01/HWRK03/functions.py
@@ -0,0 +1,35 @@
+from random import randint, random
+
+
+def task1(sze=5):
+    lst = [randint(1, 10+1) for _ in range(sze)]                 #формируем список из рандомных значений от 1 до 10
+    newlst = []
+    for i in range(sze // 2 if sze % 2 == 0 else sze // 2 + 1):  #к-во итераций цикла зависит от чётности размера списка
+        newlst.append(lst[i] * lst[sze - i - 1])                 #формируем новый список
+    print(lst, '->', newlst)
+
+
+def task2(sze=5):
+    lst = [round(random() * 10, 2) for _ in range(sze)]          #формируем список из рандомных значений от 1 до 10
+    print(lst)
+    lst = [round(lst[i] - int(lst[i]), 2) for i in range(sze)]   #изменяем список, чтоб остались только дробные части
+    print('Разница между максимальной и минимальной дробными частями =', max(lst) - min(lst)) #и монжно было спокойно использовать min max
+
+
+def task3(decnumber):
+    dcnum = []
+    while decnumber > 0:
+        dcnum.append(decnumber % 2)
+        decnumber //= 2
+    print(dcnum[::-1])
+
+
+def task4(lst, k=8):                            #список заполняется рекурсией
+    cur_len = len(lst)                          #размер списка
+    if cur_len >= k * 2:                        #берётся для проверки выхода из рекурсии
+        return lst                              #выход
+
+    lst.append(lst[cur_len - 1] + lst[cur_len - 2])      #в конец списка добавляем обычное число фибоначчи
+    lst.insert(0, (-1) ** (cur_len // 2) * lst[cur_len]) #в начало - нега
+    task4(lst, k)                                        #и поехали заново
+    return lst
---
+++ HWRK01/HWRK03/main.py
@@ -0,0 +1,19 @@
+import functions
+
+while True:
+    try:
+        num = int(input('Всего у нас 4 задачи. Какой номер решить? (для выхода введите q) ')) #вообще по любому символу работает :(
+    except ValueError:
+        print('Пойду я отсюда')
+        break
+    else:
+        if num == 1:
+            functions.task1(int(input('Какого размера список сгенерировать? ')))
+        elif num == 2:
+            functions.task2()
+        elif num == 3:
+            functions.task3(int(input('Какое число перевести в двоичное? ')))
+        elif num == 4:
+            lst_fib = [1, 0, 1]                                                              #начальное значение списка
+            print(functions.task4(lst_fib,int(input('Сколько чисел Нефигабоначчи вывести? '))))
+

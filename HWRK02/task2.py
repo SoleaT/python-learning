e61266f873b37fd0d434878181c5c9b546fcef36
Solea <cassandra01@yandex.ru>
2022-11-30 21:04:26 +0300

    дз2, задача 2, наконец-то она работает

--- HWRK02/task2.py
+++ HWRK02/task2.py
@@ -1,11 +1,13 @@
 # Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
 from math import factorial
-print([factorial(int(i)) for i in range(1,int(input('Введите число: '))+1)])
+
+print([factorial(int(i)) for i in range(1, int(input('Введите число: ')) + 1)])
 
 # \(°□° )/
 digit = int(input('Введите число: '))
-digits = list(range(1, digit+1))
-for i in range(1, len(digits)):
-    for j in range(1, i + 1):
-        digits[i] *= j
+digits = []
+mult = 1
+for i in range(1, digit+1):
+    mult*=i
+    digits.append(mult)
 print(digits)
